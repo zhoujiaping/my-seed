150bc7cac7ddd0cf6a2e69c9391e3f2d6527ca1b
zhoujiaping <421211679@qq.com>
2017-10-12 17:18:01 +0800

    test.js

--- src/main/webapp/static/test.js
+++ src/main/webapp/static/test.js
@@ -6,6 +6,7 @@ $(function(){
 		var origUrl = $('#url').text();
 		var url = howso.addSessionid(cxtPath+origUrl,$('#sid').text());
 		var str = $('#params').text()||'{}';
+		//str = str.split(/\r\n/igm).join('');
 		var data = howso.toAjaxParam(JSON.parse(str));
 		http[method](url,data,null,'text').done(res=>{
 			$('#res').text(res);
