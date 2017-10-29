9822d6280837caba640fcf432ca7e528d4ded93c
Administrator <Administrator@192.168.223.1>
2017-10-29 15:39:24 +0800

    nothing

--- src/main/java/cn/howso/deeplan/util/JSONUtils.java
+++ src/main/java/cn/howso/deeplan/util/JSONUtils.java
@@ -3,9 +3,11 @@ package cn.howso.deeplan.util;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.alibaba.fastjson.JSON;
+
 public class JSONUtils {
     public static final String toJSONString(Object obj){
-        return com.alibaba.fastjson.JSON.toJSONString(obj);
+        return JSON.toJSONString(obj);
     }
     public static void main(String[] args) {
         Map<String,String> map = new HashMap<>();
